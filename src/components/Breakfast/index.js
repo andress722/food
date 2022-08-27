6f3bda339a0f58cc250a8062acea024a30497502
Andre Moreira Cidre <andrecidre@hotmail.com>
2022-08-27 01:30:59 -0300

    alterando classes

--- src/components/Breakfast/index.js
+++ src/components/Breakfast/index.js
@@ -1,44 +1,44 @@
 export default function BreakFast(){
     return (
-        <section class="breakfast-menu">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="breakfast-menu-content">
-                        <div class="row">
-                            <div class="col-md-5">
-                                <div class="left-image">
+        <section className="breakfast-menu">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-10 col-md-offset-1">
+                    <div className="breakfast-menu-content">
+                        <div className="row">
+                            <div className="col-md-5">
+                                <div className="left-image">
                                     <img src="img/breakfast_menu.jpg" alt="Breakfast"></img>
                                 </div>
                             </div>
-                            <div class="col-md-7">
+                            <div className="col-md-7">
                                 <h2>Breakfast Menu</h2>
-                                <div id="owl-breakfast" class="owl-carousel owl-theme">
-                                    <div class="item col-md-12">
-                                        <div class="food-item">
+                                <div id="owl-breakfast" className="owl-carousel owl-theme">
+                                    <div className="item col-md-12">
+                                        <div className="food-item">
                                             <img src="img/breakfast_item.jpg" alt=""></img>
-                                            <div class="price">$3.50</div>
-                                            <div class="text-content">
+                                            <div className="price">$3.50</div>
+                                            <div className="text-content">
                                                 <h4>Kale Chips Art Party</h4>
                                                 <p>Dreamcatcher squid ennui cliche chicharros nes echo  small batch jean ditcher meal...</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="item col-md-12">
-                                        <div class="food-item">
+                                    <div className="item col-md-12">
+                                        <div className="food-item">
                                             <img src="img/lunch_item.jpg" alt=""></img>
-                                            <div class="price">$7.25</div>
-                                            <div class="text-content">
+                                            <div className="price">$7.25</div>
+                                            <div className="text-content">
                                                 <h4>Drink Vinegar Prism</h4>
                                                 <p>Dreamcatcher squid ennui cliche chicharros nes echo  small batch jean ditcher meal...</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="item col-md-12">
-                                        <div class="food-item">
+                                    <div className="item col-md-12">
+                                        <div className="food-item">
                                             <img src="img/dinner_item.jpg" alt=""></img>
-                                            <div class="price">$11.50</div>
-                                            <div class="text-content">
+                                            <div className="price">$11.50</div>
+                                            <div className="text-content">
                                                 <h4>Taiyaki Gastro Tousled</h4>
                                                 <p>Dreamcatcher squid ennui cliche chicharros nes echo  small batch jean ditcher meal...</p>
                                             </div>
--- src/components/Dinnermenu/index.js
+++ src/components/Dinnermenu/index.js
@@ -1,44 +1,44 @@
 export default function DinnerMenu(){
     return (
-        <section class="dinner-menu">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="dinner-menu-content">
-                        <div class="row">
-                            <div class="col-md-5">
-                                <div class="left-image">
+        <section className="dinner-menu">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-10 col-md-offset-1">
+                    <div className="dinner-menu-content">
+                        <div className="row">
+                            <div className="col-md-5">
+                                <div className="left-image">
                                     <img src="img/dinner_menu.jpg" alt=""></img>
                                 </div>
                             </div>
-                            <div class="col-md-7">
+                            <div className="col-md-7">
                                 <h2>Dinner Menu</h2>
-                                <div id="owl-dinner" class="owl-carousel owl-theme">
-                                    <div class="item col-md-12">
-                                        <div class="food-item">
+                                <div id="owl-dinner" className="owl-carousel owl-theme">
+                                    <div className="item col-md-12">
+                                        <div className="food-item">
                                             <img src="img/dinner_item.jpg" alt=""></img>
-                                            <div class="price">$8.25</div>
-                                            <div class="text-content">
+                                            <div className="price">$8.25</div>
+                                            <div className="text-content">
                                                 <h4>Meal Apples Almonds</h4>
                                                 <p>Dreamcatcher squid ennui cliche chicharros nes echo  small batch jean ditcher meal...</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="item col-md-12">
-                                        <div class="food-item">
+                                    <div className="item col-md-12">
+                                        <div className="food-item">
                                             <img src="img/lunch_item.jpg" alt=""></img>
-                                            <div class="price">$12.50</div>
-                                            <div class="text-content">
+                                            <div className="price">$12.50</div>
+                                            <div className="text-content">
                                                 <h4>Ditch Corn Art</h4>
                                                 <p>Dreamcatcher squid ennui cliche chicharros nes echo  small batch jean ditcher meal...</p>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="item col-md-12">
-                                        <div class="food-item">
+                                    <div className="item col-md-12">
+                                        <div className="food-item">
                                             <img src="img/breakfast_item.jpg" alt=""></img>
-                                            <div class="price">$16.00</div>
-                                            <div class="text-content">
+                                            <div className="price">$16.00</div>
+                                            <div className="text-content">
                                                 <h4>Kale Chips Art Party</h4>
                                                 <p>Dreamcatcher squid ennui cliche chicharros nes echo  small batch jean ditcher meal...</p>
                                             </div>
